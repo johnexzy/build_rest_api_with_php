969b21b31286d535c21e60d156b4d46558ceba11
Johnexzy <obajohn75@gmail.com>
2020-05-19 23:08:31 +0100

    Latest

--- src/Controller/ViewController/ViewController.php
+++ src/Controller/ViewController/ViewController.php
@@ -4,18 +4,21 @@ namespace Src\Controller\ViewController;
 
 use Src\Layout\Details;
 use Src\TableGateways\CarouselGateway;
+use Src\TableGateways\NewsGateway;
 class ViewController
 {
     private $short_url = null;
     private $id = null;
     private $root = null;
     private $group = null;
-    public function __construct($short_url, $id, $root, $group)
+    private $db = null;
+    public function __construct($short_url, $id, $root, $group, $db)
     {
         $this->short_url = $short_url;
         $this->id = $id;
         $this->root = $root;
         $this->group = $group;
+        $this->db = $db;
     }
 
     public function makeRequest()
@@ -23,7 +26,12 @@ class ViewController
         // $res = file_get_contents("http://127.0.0.1:8080/api/$this->group/$this->short_url", true, null);
         // $res = (array) json_decode($res, true);
         if ($this->group == 'carousel') {
-            $carousel = new CarouselGateway();
+            $carousel = new CarouselGateway($this->db);
+            $res = $carousel->findByUrl($this->short_url);
+        }
+        else{
+            $news = new NewsGateway($this->db);
+            $res = $news->findByUrl($this->short_url);
         }
         return $res;
     }
